57383e0a944cd8c049d2337b99732a8d9cd8ef33
Rosie Faulkner <rosiefaulkner@0069L2Z7CRMLCVF.local>
2020-10-23 10:10:07 -0400

    set up core class

--- app/libraries/core.php
+++ app/libraries/core.php
@@ -11,10 +11,15 @@ class Core {
     protected $params = [];
 
     public function __contruct(){
-        $this->getUrl();
+        print_r($this->getUrl());
     }
 
     public function getUrl(){
-        echo $_GET['url'];
+        if(isset($_GET['url'])){
+            $url = rtrim($_GET['url'], '/');
+            $url = filter_var($url, FILTER_SANITIZE_URL);
+            $url = explode('/', $url);
+            return $url;
+        }
     }
 }
