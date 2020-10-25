3eea37fe3905299ca940cb265e07933c068c2e96
Rosie Faulkner <rosiefaulkner@0069L2Z7CRMLCVF.attlocal.net>
2020-10-24 20:40:55 -0400

    loading views

--- faulknermvc/app/controllers/Pages.php
+++ faulknermvc/app/controllers/Pages.php
@@ -1,14 +1,18 @@
 <?php
   class Pages extends Controller {
     public function __construct(){
-     
+
     }
 
     public function index(){
-      
+      $data = [
+        'title' => 'Welcome'
+      ];
+
+      $this->view('pages/index', $data);
     }
 
-    public function about($id){
-      echo $id;
+    public function about(){
+      $this->view('pages/about');
     }
   }
