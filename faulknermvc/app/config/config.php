467cf0b8d8e780419cfd515e59cf6eba5c23bad6
Rosie Faulkner <rosiefaulkner@0069L2Z7CRMLCVF.attlocal.net>
2020-10-25 03:52:29 -0400

    creates db class

--- faulknermvc/app/config/config.php
+++ faulknermvc/app/config/config.php
@@ -1,4 +1,10 @@
 <?php
+  // DB Params
+  define('DB_HOST', 'localhost');
+  define('DB_USER', 'rosie');
+  define('DB_PASS', 'root');
+  define('DB_NAME', 'tmvc');
+
   // App Root
   define('APPROOT', dirname(dirname(__FILE__)));
   // URL Root
